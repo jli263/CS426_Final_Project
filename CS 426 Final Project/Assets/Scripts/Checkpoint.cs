da6eb3178c2b292b622df889f86926b9533140fe
unknown <jli263@uic.edu>
2020-04-06 20:39:37 -0500

    added patrolling AI

---
+++ CS 426 Final Project/Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    [SerializeField]
+    protected float drawRadius = 1.0f;
+
+    public virtual void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, drawRadius);
+    }
+}
---
+++ CS 426 Final Project/Assets/Scripts/SoldierPatrol.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+//Orginal code by https://www.youtube.com/watch?v=5q4JHuJAAcQ&t=332s
+
+public class SoldierPatrol : MonoBehaviour
+{
+    [SerializeField]
+    bool patrolWaiting;
+
+    [SerializeField]
+    float waitingTime;
+    
+    [SerializeField]
+    Checkpoint[] checkPoints;
+
+    [SerializeField]
+    float switchProb;
+
+    Animator animator;
+    NavMeshAgent navMeshAgent;
+    int currPatrolIndex = 0;
+    bool isMoving;
+    bool isWaiting;
+    bool originalPath = true;
+    float timeElapsed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = this.GetComponent<NavMeshAgent>();
+        animator = this.GetComponent<Animator>();
+        
+        if (navMeshAgent != null)
+        {
+            if(checkPoints.Length >= 2)
+            {
+                currPatrolIndex = 0;
+                SetDestination();
+            }
+        } 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isMoving)
+        {
+            animator.SetBool("isWalking", true);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
+
+        if (isMoving && navMeshAgent.remainingDistance <= 1.0f)
+        {
+            isMoving = false;
+
+            if (patrolWaiting)
+            {
+                timeElapsed = 0;
+                isWaiting = true;
+            }
+            else
+            {
+                ChangeCheckPoint();
+                SetDestination();
+            }
+        }
+
+        //makes the AI wait before turning 
+        if (isWaiting)
+        {
+            timeElapsed += Time.deltaTime;
+            if (timeElapsed >= waitingTime)
+            {
+                ChangeCheckPoint();
+                SetDestination();
+                isWaiting = false;
+            }
+        }
+    }
+
+    private void SetDestination()
+    {
+        //Set the destination of the AI
+        Vector3 checkpointVector = checkPoints[currPatrolIndex].transform.position;
+        navMeshAgent.SetDestination(checkpointVector);
+        isMoving = true;
+    }
+
+    private void ChangeCheckPoint ()
+    {
+        //Determine if change of course
+        if (UnityEngine.Random.Range(0f,1f) <= switchProb)
+        {
+            originalPath = !originalPath;
+        }
+        if (originalPath)
+        {
+            currPatrolIndex++;
+
+            //Check if out of bounds
+            if (currPatrolIndex >= checkPoints.Length)
+            {
+                //Go back to the previous check point
+                currPatrolIndex = checkPoints.Length - 2;
+                originalPath = !originalPath;
+            }
+        }
+        else
+        {
+            //Check if out of bounds
+            if (--currPatrolIndex < 0)
+            {
+                //Go back to the previous check point
+                currPatrolIndex = 1;
+                originalPath = !originalPath;
+            }
+        }
+    }
+}
