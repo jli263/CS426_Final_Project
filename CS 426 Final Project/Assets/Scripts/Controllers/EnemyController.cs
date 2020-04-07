e08230fe0cf7b6ec75151507ea9d17e1d9329d7f
anaran7 <44587677+anaran7@users.noreply.github.com>
2020-04-06 19:17:26 -0500

    Added improved enemy detection and civilian ai

--- CS 426 Final Project/Assets/Scripts/Controllers/EnemyController.cs
+++ CS 426 Final Project/Assets/Scripts/Controllers/EnemyController.cs
@@ -2,31 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Audio;
 
 public class EnemyController : MonoBehaviour
 {
 
-    public float lookRadius = 10f;
-    static Animator anim;
-    public AudioSource audio1;
-    public AudioSource audio2;
-    public bool audio = false;
+   // public float lookRadius = 10f;
+    //public AudioSource audio1;
+    //public AudioSource audio2;
+    public bool isAudioPlaying = false;
+    public AudioSource audioSound;
+    //public Animator anim;
 
     private const float timeToCharge = 10.5f;
     private float chargeTimer = 0.0f;
+
+    public SphereCollider detectionRange;
 
     Transform target;
     NavMeshAgent agent;
     // Start is called before the first frame update
     void Start()
     {
+        detectionRange = GetComponent<SphereCollider>();
         target = PlayerManager.instance.player.transform;
         agent = GetComponent<NavMeshAgent>();
-        anim = GetComponent<Animator>();
-        //audio2 = GetComponent<AudioSource>();
+      //  anim = GetComponent<Animator>();
+        audioSound = GetComponent<AudioSource>();
+       // anim.SetBool("isIdle", true);
     }
 
     // Update is called once per frame
+    /*
     void Update()
     {
         //yelling.Play();
@@ -56,10 +63,45 @@ public class EnemyController : MonoBehaviour
             chargeTimer = 0.0f;
         }
     }
+    */
 
-    void OnDrawGizmosSelected()
+   // void OnDrawGizmosSelected()
+   // {
+   //     Gizmos.color = Color.red;
+   //     Gizmos.DrawWireSphere(transform.position, lookRadius);
+   // }
+
+
+    void OnTriggerStay(Collider other)
     {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, lookRadius);
+        if (other.gameObject.CompareTag("Player"))
+        {
+            //get the angle of where the player was detected.
+            Vector3 direction = other.transform.position - transform.position;
+            float angle = Vector3.Angle(direction, transform.forward);
+
+            //check if the player is within the angle of sight
+            if (angle < 60.0f * 0.5f)
+            {
+                RaycastHit hit;
+                if (Physics.Raycast(transform.position + Vector3.up, direction, out hit, detectionRange.radius))
+                {
+                    if (hit.collider.gameObject.CompareTag("Player"))
+                    {
+                        Debug.DrawRay(transform.position + Vector3.up, direction, Color.red);
+                        Debug.Log("Playerfound");
+                        //audioSound.PlayOneShot(audioSound.GetComponent<AudioClip>());
+                      //  anim.SetBool("isIdle", false);
+                        agent.SetDestination(target.position);
+                        if(isAudioPlaying == false)
+                        {
+                            isAudioPlaying = true;
+                            audioSound.Play();
+                        }
+                        
+                    }
+                }
+            }
+        }
     }
 }
---
+++ CS 426 Final Project/Assets/Scripts/WanderAI.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WanderAI : MonoBehaviour
+{
+    public float moveSpeed = 1f;
+    public float rotSpeed = 80f;
+
+    private bool isWandering = false;
+    private bool isRotatingLeft = false;
+    private bool isRotatingRight = false;
+    private bool isWalking = false;
+
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.SetBool("isIdle", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Debug.DrawRay(transform.position + Vector3.up, transform.TransformDirection(Vector3.forward), Color.red);
+
+        if (isWandering == false)
+        {
+            StartCoroutine(Wander());
+        }
+
+        if (isRotatingRight == true)
+        {
+            transform.Rotate(transform.up * Time.deltaTime * rotSpeed);
+        }
+
+        if (isRotatingLeft == true)
+        {
+            transform.Rotate(transform.up * Time.deltaTime * -rotSpeed);
+        }
+        if (isWalking == true)
+        {
+            transform.position += transform.forward * moveSpeed * Time.deltaTime;
+        }
+
+    }
+
+    IEnumerator Wander()
+    {
+        int rotTime = Random.Range(1, 3);
+        int rotateWait = Random.Range(1, 3);
+        int rotateLorR = Random.Range(0, 3);
+        int walkWait = Random.Range(1, 3);
+        int walkTime = Random.Range(1, 4);
+
+        isWandering = true;
+        anim.SetBool("isIdle", true);
+        yield return new WaitForSeconds(walkWait);
+        RaycastHit hit;
+        if (!Physics.Raycast(transform.position + Vector3.up, transform.TransformDirection(Vector3.forward), out hit, 2))
+        {
+            anim.SetBool("isIdle", false);
+            isWalking = true;
+            yield return new WaitForSeconds(walkTime);
+        }
+        anim.SetBool("isIdle", true);
+        isWalking = false;
+        yield return new WaitForSeconds(rotateWait / 2);
+
+        if (rotateLorR == 1)
+        {
+            isRotatingRight = true;
+            yield return new WaitForSeconds(rotTime);
+            isRotatingRight = false;
+
+        }
+        else
+        {
+            isRotatingLeft = true;
+            yield return new WaitForSeconds(rotTime);
+            isRotatingLeft = false;
+        }
+        isWandering = false;
+    }
+}
