97bc675276304c7f2b4fa6874b4a266d72f611e5
unknown <jli263@uic.edu>
2020-04-13 21:03:01 -0500

    added pause menu, end game menu, end game colliders, and more soldiers

---
+++ CS 426 Final Project/Assets/Scripts/EscapeMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EscapeMenu : MonoBehaviour
+{
+
+    public GameObject escapePanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        escapePanel.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+            escapePanel.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("GameScene"); 
+    }
+}
---
+++ CS 426 Final Project/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public GameObject menuPanel;
+
+    private bool gamePaused = false;
+
+    private void Start()
+    {
+        menuPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gamePaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void ResumeGame()
+    {
+        menuPanel.SetActive(false);
+        gamePaused = false;
+        Time.timeScale = 1f;
+    }
+
+    void PauseGame()
+    {
+        menuPanel.SetActive(true);
+        gamePaused = true;
+        Time.timeScale = 0f;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
