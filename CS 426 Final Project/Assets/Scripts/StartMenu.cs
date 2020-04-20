d24c9538af44a44af13e6278d3d0f2f2a73a0d9a
unknown <jli263@uic.edu>
2020-04-20 12:04:02 -0500

    Added start menu

---
+++ CS 426 Final Project/Assets/Scripts/StartMenu.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class StartMenu : MonoBehaviour
+{
+
+    public GameObject controlPanel;
+    public GameObject storyPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       // controlPanel.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void TutorialGame()
+    {
+        SceneManager.LoadScene("TutorialScene");
+    }
+
+    public void LoadControls()
+    {
+        controlPanel.SetActive(true);
+    }
+
+    public void returnFromControls()
+    {
+        controlPanel.SetActive(false);
+    }
+
+    public void LoadStory()
+    {
+        storyPanel.SetActive(true);
+    }
+
+    public void returnFromStory()
+    {
+        storyPanel.SetActive(false);
+    }
+}
