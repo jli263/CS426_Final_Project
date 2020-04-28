4f685b7c2eaa60238ff517ee60eda2650ab6e4ac
unknown <jli263@uic.edu>
2020-04-27 20:28:34 -0500

    added text to tutorial and added more props

---
+++ CS 426 Final Project/Assets/Scripts/Tutorial.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Tutorial : MonoBehaviour
+{
+    public static string displayText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    public void changeText()
+    {
+     //   gameObject.GetComponent<UnityEngine.UI.Text>().text = "Find the ;
+    }
+}
